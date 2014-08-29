7c86749a7a4c738df9c80658dfe5e12b6e519fb8
zzuutt <zzuutt34@free.fr>
2014-08-29 10:44:49 +0200

    update

--- I18n/AdminIncludes/fr_FR.php
+++ I18n/AdminIncludes/fr_FR.php
@@ -1,5 +1,9 @@
 <?php
 
 return array(
-
+    'Google / Universal Analytics setup' => 'Parametres Google / Universal Analytics',
+    'IdValue' => 'Identifiant',
+    'Save' => 'Enregistrer',
+    'TypeValue' => 'Compte',
+    'Your ID analytics' => 'Votre identifiant analytics',
 );
--- I18n/fr_FR.php
+++ I18n/fr_FR.php
@@ -3,7 +3,8 @@
 return array(
     'Can\'t read file ' => 'impossible de lire le fichier',
     'Can\'t write file ' => 'impossible d\'ecrire le fichier',
-    'TypeValue' => 'Choix Analytics',
+    'IdValue' => 'identifiant',
     'The file ' => 'le fichier',
     'The file is corrupted.' => 'Le fichier est corrompu',
+    'TypeValue' => 'Choix Analytics',
 );
