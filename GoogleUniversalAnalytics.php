67918274bb8eba15f16dd54e5399df3c67abe765
zzuutt <zzuutt34@free.fr>
2014-08-29 10:34:24 +0200

    update

--- GoogleUniversalAnalytics.php
+++ GoogleUniversalAnalytics.php
@@ -1,8 +1,14 @@
 <?php
 /*************************************************************************************/
 /*                                                                                   */
+/*     Copyright (c) zzuutt                                                          */
+/*     email : zzuutt34@free.fr                                                      */ 
 /*                                                                                   */
-/*      email : zzuutt34@free.fr                                                     */
+/*     This program is free software; you can redistribute it and/or modify          */
+/*     it under the terms of the GNU General Public License as published by          */
+/*     the Free Software Foundation; either version 3 of the                         */
+/*     GNU General Public License : http://www.gnu.org/licenses/                     */
+/*                                                                                   */
 /*                                                                                   */
 /*************************************************************************************/
 
--- Loop/CheckRightsLoop.php
+++ Loop/CheckRightsLoop.php
@@ -1,13 +1,15 @@
 <?php
 /*************************************************************************************/
-/*      This file is part of the Thelia package.                                     */
 /*                                                                                   */
-/*      Copyright (c) OpenStudio                                                     */
-/*      email : dev@thelia.net                                                       */
-/*      web : http://www.thelia.net                                                  */
+/*     Copyright (c) zzuutt                                                          */
+/*     email : zzuutt34@free.fr                                                      */ 
 /*                                                                                   */
-/*      For the full copyright and license information, please view the LICENSE.txt  */
-/*      file that was distributed with this source code.                             */
+/*     This program is free software; you can redistribute it and/or modify          */
+/*     it under the terms of the GNU General Public License as published by          */
+/*     the Free Software Foundation; either version 3 of the                         */
+/*     GNU General Public License : http://www.gnu.org/licenses/                     */
+/*                                                                                   */
+/*                                                                                   */
 /*************************************************************************************/
 namespace GoogleUniversalAnalytics\Loop;
 
